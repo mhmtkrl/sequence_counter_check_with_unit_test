c37ebcbd2734ba22833c16f0140fe75f46a3d9bb
mhmtkrl <koral.mehmet45@gmail.com>
2022-10-24 23:25:09 +0300

    basic structure was created for handling process

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <stdint.h>
 
 uint8_t testVector[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 
-int main() {
+typedef void (*FsequenceCheckFunction)(uint8_t *);
 
+typedef struct {
+    uint8_t errorCount;
+    FsequenceCheckFunction checkSequenceFunction;
+}SEQUENCE_COUNTER_CHECK_t;
+
+SEQUENCE_COUNTER_CHECK_t t_sequence_check;
+SEQUENCE_COUNTER_CHECK_t *sequence_check_struct = &t_sequence_check;
+
+void checkSequence(uint8_t *testVector) {
+
+}
+
+void structInit(void) {
+    sequence_check_struct->errorCount = 0;
+    sequence_check_struct->checkSequenceFunction = &checkSequence;
+}
+
+int main() {
+    structInit();
     printf("Hi\r\n");
 }
