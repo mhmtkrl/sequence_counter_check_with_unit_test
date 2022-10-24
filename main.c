4454fc8da8a4b7478a6e5acdce2900f1c36af467
mhmtkrl <koral.mehmet45@gmail.com>
2022-10-25 00:42:43 +0300

    func return is struct

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdint.h>
-
+#include <assert.h>
 #define testVectorLength    13
 
-uint8_t testVector[testVectorLength] = {3, 4, 2, 9, 5, 0, 7, 8, 9, 13, 14, 15, 0};
+uint8_t testVector[testVectorLength] = {3, 4, 4, 9, 5, 5, 7, 8, 9, 13, 14, 15, 0};
 
-typedef void (*FsequenceCheckFunction)(uint8_t);
+typedef struct {
+    uint8_t repetation;
+    uint8_t loss;
+}ERROR_COUNT_t;
+
+ERROR_COUNT_t globalEC = {0, 0};
+
+typedef ERROR_COUNT_t (*FsequenceCheckFunction)(uint8_t);
 
 typedef enum {
     REPETATION_OF_INFORMATION,
     LOSS_OF_INFORMATION,
     NO_ERROR
 }COMMUNICATION_FAULTS_t;
-
-typedef struct {
-    uint8_t repetation;
-    uint8_t loss;
-}ERROR_COUNT_t;
 
 typedef struct {
     ERROR_COUNT_t errorCount;
@@ -27,7 +29,8 @@ typedef struct {
 SEQUENCE_COUNTER_CHECK_t t_sequence_check;
 SEQUENCE_COUNTER_CHECK_t *sequence_check_struct = &t_sequence_check;
 
-void checkSequence(uint8_t testNumber) {
+ERROR_COUNT_t checkSequence(uint8_t testNumber) {
+    static ERROR_COUNT_t ec;
     static uint8_t prevNumber;
     static int8_t isFirstTime = 1;
     if(isFirstTime) {
@@ -55,6 +58,10 @@ void checkSequence(uint8_t testNumber) {
 
     prevNumber = currentNumber;
     printf("---------------------------\r\n");
+    ec.loss = sequence_check_struct->errorCount.loss;
+    ec.repetation = sequence_check_struct->errorCount.repetation;
+
+    return ec;
 }
 
 void structInit(void) {
@@ -68,7 +75,7 @@ int main() {
 
 
     for(int i = 0 ; i < testVectorLength ; i++) {
-        sequence_check_struct->checkSequenceFunction(testVector[i]);
+        globalEC = sequence_check_struct->checkSequenceFunction(testVector[i]);
     }
 
     printf("done!");
