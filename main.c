67a2b22b3ac95c03ca7622d3a6002ce07ac6a899
mhmtkrl <koral.mehmet45@gmail.com>
2022-10-24 23:34:14 +0300

    first check is okay

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define testVectorLength    8
+
 uint8_t testVector[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 
-typedef void (*FsequenceCheckFunction)(uint8_t *);
+typedef void (*FsequenceCheckFunction)(uint8_t);
 
 typedef struct {
     uint8_t errorCount;
@@ -13,8 +15,16 @@ typedef struct {
 SEQUENCE_COUNTER_CHECK_t t_sequence_check;
 SEQUENCE_COUNTER_CHECK_t *sequence_check_struct = &t_sequence_check;
 
-void checkSequence(uint8_t *testVector) {
+void checkSequence(uint8_t testNumber) {
+    static uint8_t prevNumber = 0;
+    uint8_t currentNumber = testNumber;
+    printf("current Number: %d\r\n", currentNumber);
+    printf("Previous Number: %d\r\n", prevNumber);
+    int8_t diff = currentNumber - prevNumber;
+    printf("diff: %d\r\n", diff);
 
+    prevNumber = currentNumber;
+    printf("---------------------------\r\n");
 }
 
 void structInit(void) {
@@ -24,5 +34,9 @@ void structInit(void) {
 
 int main() {
     structInit();
-    printf("Hi\r\n");
+
+
+    for(int i = 0 ; i < testVectorLength ; i++) {
+        sequence_check_struct->checkSequenceFunction(testVector[i]);
+    }
 }
